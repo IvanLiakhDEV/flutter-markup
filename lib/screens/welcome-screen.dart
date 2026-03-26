f80752bc043943acf7bdc990cf3e2528d121f778
IvanLiakh1 <liakh.ivan1@student.uzhnu.edu.ua>
2026-03-26 11:28:48 +0200

    Оновлено WelcomeScreen

--- lib/screens/welcome-screen.dart
+++ lib/screens/welcome-screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_markup/core/app-style.dart';
+import 'package:flutter_markup/core/extensions.dart';
 import 'package:flutter_markup/widgets/app-button.dart';
 import '../widgets/app-logo.dart';
 
@@ -10,35 +11,39 @@ class Welcomescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 235),
-            AppLogo(size: 138, theme: 'light'),
-            const SizedBox(height: 84),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40),
-              child: Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                textAlign: TextAlign.center,
-                style: AppTextStyles.sm,
-              ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 52),
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 400),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: 235 - context.paddingTop),
+                AppLogo(size: 138, theme: 'light'),
+                const SizedBox(height: 84),
+                Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                  textAlign: TextAlign.center,
+                  style: AppTextStyles.sm,
+                ),
+                const SizedBox(height: 34),
+                AppButton(
+                  text: 'Log in',
+                  backgroundColor: AppColors.scaffoldBackground,
+                  textColor: Colors.white,
+                  onPressed: () => print('Log in'),
+                ),
+                const SizedBox(height: 7),
+                AppButton(
+                  text: 'Sign Up',
+                  backgroundColor: AppColors.surfaceBackground,
+                  textColor: AppColors.textPrimary,
+                  onPressed: () => print('Sign Up'),
+                ),
+              ],
             ),
-            const SizedBox(height: 34),
-            AppButton(
-              text: 'Log in',
-              backgroundColor: AppColors.scaffoldBackground,
-              textColor: Colors.white,
-              onPressed: () => print('Log in'),
-            ),
-            const SizedBox(height: 7),
-            AppButton(
-              text: 'Sign Up',
-              backgroundColor: AppColors.surfaceBackground,
-              textColor: AppColors.textPrimary,
-              onPressed: () => print('Sign Up'),
-            ),
-          ],
+          ),
         ),
       ),
     );
