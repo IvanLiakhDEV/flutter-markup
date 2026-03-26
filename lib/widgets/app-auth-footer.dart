c17269310e2bcbae3392fad8cc3df9506c98d920
IvanLiakh1 <liakh.ivan1@student.uzhnu.edu.ua>
2026-03-26 19:57:20 +0200

    Створено віджет AppAuthFooter

---
+++ lib/widgets/app-auth-footer.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_markup/core/app-style.dart';
+import 'package:flutter_markup/widgets/app-auth-method-button.dart';
+import 'package:flutter_markup/widgets/app-button.dart';
+
+class AuthFooter extends StatelessWidget {
+  final String buttonText;
+  final VoidCallback onButtonPressed;
+  final String? socialText;
+  final bool showSocialIcons;
+  final String bottomTextSuffix;
+  final String bottomLinkText;
+  final VoidCallback onBottomLinkPressed;
+  final Widget? extraWidget;
+  const AuthFooter({
+    super.key,
+    required this.buttonText,
+    required this.onButtonPressed,
+    this.socialText,
+    this.showSocialIcons = true,
+    required this.bottomTextSuffix,
+    required this.bottomLinkText,
+    required this.onBottomLinkPressed,
+    this.extraWidget,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        if (extraWidget != null) ...[extraWidget!, const SizedBox(height: 20)],
+        AppButton(
+          text: buttonText,
+          backgroundColor: AppColors.scaffoldBackground,
+          textColor: Colors.white,
+          onPressed: onButtonPressed,
+        ),
+        if (showSocialIcons) ...[
+          const SizedBox(height: 16),
+          Text(
+            socialText ?? 'or sign up with',
+            style: AppTextStyles.sm,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 12),
+          const Center(
+            child: SizedBox(
+              width: 160,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AppAuthMethodButton(icon: 'google'),
+                  AppAuthMethodButton(icon: 'facebook'),
+                  AppAuthMethodButton(icon: 'fingerprint'),
+                ],
+              ),
+            ),
+          ),
+        ],
+        const SizedBox(height: 38),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('$bottomLinkText ', style: AppTextStyles.sm),
+            GestureDetector(
+              onTap: onBottomLinkPressed,
+              child: Text(
+                bottomTextSuffix,
+                style: AppTextStyles.sm.copyWith(
+                  color: AppColors.textPrimary,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
